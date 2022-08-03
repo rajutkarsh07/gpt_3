52bf518321493af8ad05934b7c7982411861bc92
utkarsh_raj <rajutkarsh2505@gmail.com>
2022-08-04 02:36:26 +0530

    about section added

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./feature.css";
 
 const Feature = ({ title, text }) => (
-  <div className="gpt3__features-container__feature">Feature</div>
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
 );
 
 export default Feature;
