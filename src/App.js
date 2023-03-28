634f2d45ab7147905f8fc6ef09a5b67b3804ac4a
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-03-28 22:09:55 +0530

    refactoring changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-import { CTA, Brand, Navbar } from './components';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
-import './App.css';
+import "./App.css";
 
 const App = () => (
   <div className="App">
